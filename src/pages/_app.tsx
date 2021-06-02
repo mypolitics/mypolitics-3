9f78849ce7b817603951231fae62493407c7275d
Antoni Kepinski <antoni@kepinski.ch>
2021-06-02 12:23:12 +0200

    Allow only 1 toast at a time

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "@services/apollo";
-import { Toaster } from "react-hot-toast";
+import { Toaster, toast, useToasterStore } from "react-hot-toast";
 import Layout from "@layout";
 import Router from "next/router";
 import NProgress from "nprogress";
 import "modern-normalize/modern-normalize.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from '@fortawesome/fontawesome-svg-core';
+import { config } from "@fortawesome/fontawesome-svg-core";
 import { FacebookProvider } from "react-facebook";
 import useTranslation from "next-translate/useTranslation";
 
@@ -22,6 +22,14 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   const { lang } = useTranslation();
   const apolloClient = useApollo(pageProps.initialApolloState);
   const facebookLanguage = `${lang}_${lang.toUpperCase()}`;
+  const { toasts } = useToasterStore();
+
+  useEffect(() => {
+    toasts
+      .filter((t) => t.visible)
+      .filter((_, i) => i >= 1)
+      .forEach((t) => toast.dismiss(t.id));
+  }, [toasts]);
 
   return (
     <FacebookProvider appId="4144384798967211" language={facebookLanguage}>
